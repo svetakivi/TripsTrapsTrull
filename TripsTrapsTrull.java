29ee3d8a357a06156cc6b2a5334f59cf112ef894
svetakivi <svetlana.kiviberg@gmail.com>
2016-01-24 09:37:33 +0200

    Create TripsTrapsTrull.java

---
+++ TripsTrapsTrull.java
@@ -0,0 +1,16 @@
+package tripstrapstrull;
+
+import javax.swing.JFrame;
+
+public class TripsTrapsTrull 
+{
+    public static void main(String[] args) 
+    {
+        JFrame TripsTrapsTrull = new TripsTrapsTrullRaam();
+        TripsTrapsTrull.setTitle("Trips traps trull");
+        TripsTrapsTrull.setSize(600, 600);
+        TripsTrapsTrull.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        TripsTrapsTrull.setLocationRelativeTo(null);
+        TripsTrapsTrull.setVisible(true);
+    }
+} // end class TripsTrapsTrull
